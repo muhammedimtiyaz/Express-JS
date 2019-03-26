a64ca18568d8b2cb6574e2d633e5136963bd21ab
Muhammed Imtiyaz <muhammedimtiyaz87@gmail.com>
2019-03-26 11:41:40 -0400

    learn middleware and order of middleware calls

--- app.js
+++ app.js
@@ -2,30 +2,53 @@ const express = require('express');
 const app = express();
 const index = require('./routes/index_routes');
 
-app.use('/index', index);
+// app.use('/index', index);
 
-app.get('/', function(req, res) {
-  res.send('Hello world!');
+// app.get('/', function(req, res) {
+//   res.send('Hello world!');
+// });
+
+// app.post('/hello', function(req, res) {
+//   res.send("you just called the post method at '/hello'!\n");
+// });
+
+// app.all('/test', function(req, res) {
+//   res.send("HTTP request does not affect this route.\n");
+// });
+
+// app.get('/:id', function(req, res) {
+//   res.send('the id you specified is ' + req.params.id);
+// });
+
+// app.get('/things/:id[0-9]{5}', function(req, res) {
+//   res.send('The id you specified is ' + req.params.id);
+// });
+
+// app.get('/things/:name/:id', function(req,res) {
+//   res.send('id: ' + req.params.id + " and name: " + req.params.name);
+// });
+
+// app.use('/things', function(req, res, next) {
+//   console.log('A request for things has been made at ' + Date.now());
+//   next();
+// });
+
+// app.get('/things', function(req, res) {
+//   res.send('Things');
+// });
+
+app.use(function(req, res, next) {
+  console.log('Start');
+  next();
 });
 
-app.post('/hello', function(req, res) {
-  res.send("you just called the post method at '/hello'!\n");
+app.get('/', function(req, res, next) {
+  res.send('Middle');
+  next();
 });
 
-app.all('/test', function(req, res) {
-  res.send("HTTP request does not affect this route.\n");
-});
-
-app.get('/:id', function(req, res) {
-  res.send('the id you specified is ' + req.params.id);
-});
-
-app.get('/things/:id[0-9]{5}', function(req, res) {
-  res.send('The id you specified is ' + req.params.id);
-})
-
-app.get('/things/:name/:id', function(req,res) {
-  res.send('id: ' + req.params.id + " and name: " + req.params.name);
+app.use('/', function(req, res) {
+  console.log('End');
 });
 
 app.get('*', function(req, res) {
